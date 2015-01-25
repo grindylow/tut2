f22bbef771375bf8ae86c101e86db17551445d5f
Martin Grill <martin.grill@gmx.de>
2015-01-25 13:24:05 +0100

    refactoring pt 2

--- tut2app/tut2/static/tut2app.js
+++ tut2app/tut2/static/tut2app.js
@@ -1,59 +1,56 @@
 /* tut2 app */
 
+'use strict';
+
 var myview;
 
 (function() {
 
-  'use strict';
-  
-    
     if(localStorage) {
-	console.log("localStorage exists");
+    console.log("localStorage exists");
     }
 
     if(localStorage.tut_grill_entries) {
-	console.log("localStorage has a tut_grill_entries entry");
-	mymodel.readFromLocalStorage();
+    console.log("localStorage has a tut_grill_entries entry");
+    mymodel.readFromLocalStorage();
     } else {
-	var sampleEntries= [
-	    { "uid":"uid1","starttime_utc_ms":1234567891, "project":"ORG.OG3.301", "logentry":"researched multiplexers", "duration":"unknown" },
-	    { "uid":"uid2","starttime_utc_ms":129566989, "starttime":"23:45", "project":"DIM.WHIT.700", "logentry":"entered time continuum", "duration":"unknown" },
-	    { "uid":"uid3","starttime_utc_ms":124566989, "starttime":"23:45", "project":"DIM.WHIT.700", "logentry":"defibrillation unit (pt 1)", "duration":"unknown" },
-	    { "uid":"uid4","starttime_utc_ms":123456, "starttime":"34:56", "project":"DIM.WHIT.300", "logentry":"researched multiplexers again", "duration":"0:35:02" }
-	];
-	localStorage.tut_grill_entries=JSON.stringify(sampleEntries);
-	mymodel.readFromLocalStorage();
+    var sampleEntries= [
+        { "uid":"uid1","starttime_utc_ms":1234567891, "project":"ORG.OG3.301", "logentry":"researched multiplexers", "duration":"unknown" },
+        { "uid":"uid2","starttime_utc_ms":129566989, "starttime":"23:45", "project":"DIM.WHIT.700", "logentry":"entered time continuum", "duration":"unknown" },
+        { "uid":"uid3","starttime_utc_ms":124566989, "starttime":"23:45", "project":"DIM.WHIT.700", "logentry":"defibrillation unit (pt 1)", "duration":"unknown" },
+        { "uid":"uid4","starttime_utc_ms":123456, "starttime":"34:56", "project":"DIM.WHIT.300", "logentry":"researched multiplexers again", "duration":"0:35:02" }
+    ];
+    localStorage.tut_grill_entries=JSON.stringify(sampleEntries);
+    mymodel.readFromLocalStorage();
     }
 
-
-
     $(document).ready(function(){
-	console.log("mark B");
-	myview=tut2_createDefaultView();
+    console.log("mark B");
+    myview=tut2_createDefaultView();
 
-	// remove this again in the end
-	var c=$('#tut-entries-container');
-	c.empty();
+    // remove this again in the end
+    var c=$('#tut-entries-container');
+    c.empty();
 
-	$("#updategui").on('click',null,null,function() {
-	    myview.redrawTutEntriesUI([mymodel.createTemplateEntry()].concat(mymodel.getAllEntries()));
-	});
+    $("#updategui").on('click',null,null,function() {
+        myview.redrawTutEntriesUI([mymodel.createTemplateEntry()].concat(mymodel.getAllEntries()));
+    });
 
-	// update entire UI every second. we'll see how well this works...
-	// don't even mention race conditions... although i think there might
-	// be *none* as javascript is "run to completion" (isn't it?).
-	// this makes the browser eat up significant CPU power 
-	// (on my system: 30% compared to <10%) - but this is with
-	// developer tools open. Drops to <10% with developer tools closed.
-	// so we should probably optimise this away in a release version.
-	// for each view, it is clear that, in general, we only need to 
-	// update the template timestamp and the duration of the first 
-	// entry. Except on boundaries (new "day" in configured timezone),
-	// but we can code in those special cases.
-	// Still, even our naive "always sync everything" approach works.
-	setInterval(function(){
-	    myview.redrawTutEntriesUI([mymodel.createTemplateEntry()].concat(mymodel.getAllEntries()));
-	},100000);
+    // update entire UI every second. we'll see how well this works...
+    // don't even mention race conditions... although i think there might
+    // be *none* as javascript is "run to completion" (isn't it?).
+    // this makes the browser eat up significant CPU power 
+    // (on my system: 30% compared to <10%) - but this is with
+    // developer tools open. Drops to <10% with developer tools closed.
+    // so we should probably optimise this away in a release version.
+    // for each view, it is clear that, in general, we only need to 
+    // update the template timestamp and the duration of the first 
+    // entry. Except on boundaries (new "day" in configured timezone),
+    // but we can code in those special cases.
+    // Still, even our naive "always sync everything" approach works.
+    setInterval(function(){
+        myview.redrawTutEntriesUI([mymodel.createTemplateEntry()].concat(mymodel.getAllEntries()));
+    },100000);
 
     });
 
--- tut2app/tut2/static/tut2model.js
+++ tut2app/tut2/static/tut2model.js
@@ -11,23 +11,28 @@
 // It also handles internal details such as marking itself as
 // modified/pristine. 
 // A TutEntry is part of the Tut data model.
-function TutEntry() {
+function tut2_createTutEntry(params) {
+    o={};
 
-    function generateUUID() {
-	// from: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	    var r = (d + Math.random()*16)%16 | 0;
-	    d = Math.floor(d/16);
-	    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	});
-	return uuid;
+    /* private variables and member functions */
+    var _starttime_utc_ms=-3;
+    var _uid=genererateUUID();
+    var _logenty='newly created logentry by TutEntry constructor';
+    var _project='newly created project by TutEntry constructor';
+
+    var generateUUID=function() {
+        // from: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
     };
 
-    this.starttime_utc_ms=-2;
-    this.logenty='newly created by TutEntry constructor';
-    this.uid=genererateUUID();
     console.log("LogEntry constructor done.");
+    return o;
 };
 
 var mymodel={
@@ -35,106 +40,106 @@ var mymodel={
     datastore:[],
 
     generateUUID:function() {
-	// from: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	    var r = (d + Math.random()*16)%16 | 0;
-	    d = Math.floor(d/16);
-	    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	});
-	return uuid;
+        // from: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
     },
 
     createTemplateEntry:function() {
-	return { "uid":"entrytemplate",
-		 "starttime_utc_ms":Date.now(),   // was: now,
-		 "project":"enter project", 
-		 "logentry":"enter log entry (task description)", 
-		 "duration":"unknown" 
-	       };
+        return { "uid":"entrytemplate",
+                 "starttime_utc_ms":Date.now(),   // was: now,
+                 "project":"enter project", 
+                 "logentry":"enter log entry (task description)", 
+                 "duration":"unknown" 
+        };
     },
     
     addEntry:function(entry) { 
-	// for now, we always add the new entry to the beginning
-	// of the list, assuming it is the most recent one.
-	// @returns UID of created entry
-	var uid=this.generateUUID();
-	entry["uid"]=uid;
-	this.datastore=[entry].concat(this.datastore);
-	this.saveToLocalStorage();
-	return uid;
+        // for now, we always add the new entry to the beginning
+        // of the list, assuming it is the most recent one.
+        // @returns UID of created entry
+        var uid=this.generateUUID();
+        entry["uid"]=uid;
+        this.datastore=[entry].concat(this.datastore);
+        this.saveToLocalStorage();
+        return uid;
     },
 
     deleteEntry:function(uid) { 
-	for(var i=0;i<this.datastore.length;i++) {
-	    if(this.datastore[i].uid==uid) {
-		console.log("found culprit for getEntryByUID");
-		this.datastore.splice(i,1); // remove 1 element at index i
-		this.saveToLocalStorage();
-		return;
-	    }
-	}
-	console.log("CANNOT FIND ENTRY WITH UID "+uid+" IN MODEL");
+        for(var i=0;i<this.datastore.length;i++) {
+            if(this.datastore[i].uid==uid) {
+            console.log("found culprit for getEntryByUID");
+            this.datastore.splice(i,1); // remove 1 element at index i
+            this.saveToLocalStorage();
+            return;
+            }
+        }
+        console.log("CANNOT FIND ENTRY WITH UID "+uid+" IN MODEL");
     },
 
     updateEntry:function(entry) {
-	for(var i=0;i<this.datastore.length;i++) {
-	    if(this.datastore[i].uid==entry.uid) {
-		console.log("found culprit for updateentry");
-		this.datastore[i]=entry;
-		break;
-	    }
-	}
-	this.saveToLocalStorage();
+        for(var i=0;i<this.datastore.length;i++) {
+            if(this.datastore[i].uid==entry.uid) {
+            console.log("found culprit for updateentry");
+            this.datastore[i]=entry;
+            break;
+            }
+        }
+        this.saveToLocalStorage();
     },
 
     getEntryByStarttime:function(starttime_utc_ms) {
-	console.log("model.getEntryByStarttime("+starttime_utc_ms+")");
-	console.log(this.datastore.length);
-	for(var i=0;i<this.datastore.length;i++) {
-	    console.log(this.datastore[i].starttime_utc_ms);
-	    if(this.datastore[i].starttime_utc_ms==starttime_utc_ms) {
-		console.log("found culprit for getEntryByStarttime");
-		return this.datastore[i];
-	    }
-	}
+        console.log("model.getEntryByStarttime("+starttime_utc_ms+")");
+        console.log(this.datastore.length);
+        for(var i=0;i<this.datastore.length;i++) {
+            console.log(this.datastore[i].starttime_utc_ms);
+            if(this.datastore[i].starttime_utc_ms==starttime_utc_ms) {
+            console.log("found culprit for getEntryByStarttime");
+            return this.datastore[i];
+            }
+        }
     },
 
     getEntryByUID:function(uid) {
-	console.log("model.getEntryByUID("+uid+")");
-	console.log(this.datastore.length);
-	for(var i=0;i<this.datastore.length;i++) {
-	    console.log(this.datastore[i].uid);
-	    if(this.datastore[i].uid==uid) {
-		console.log("found culprit for getEntryByUID");
-		return this.datastore[i];
-	    }
-	}
-	console.log("CANNOT FIND ENTRY WITH UID "+uid+" IN MODEL");
+        console.log("model.getEntryByUID("+uid+")");
+        console.log(this.datastore.length);
+        for(var i=0;i<this.datastore.length;i++) {
+            console.log(this.datastore[i].uid);
+            if(this.datastore[i].uid==uid) {
+            console.log("found culprit for getEntryByUID");
+            return this.datastore[i];
+            }
+        }
+        console.log("CANNOT FIND ENTRY WITH UID "+uid+" IN MODEL");
     },
 
     // return all Entries in descending start time order
     getAllEntries:function() {
-	this.sortMyEntriesByStarttime();
-	return this.datastore;
+        this.sortMyEntriesByStarttime();
+        return this.datastore;
     },
 
     sortMyEntriesByStarttime:function() {
-	this.datastore.sort(function(a,b){
-	    if(a.starttime_utc_ms<b.starttime_utc_ms) {
-		return 1;
-	    } else if(a.starttime_utc_ms>b.starttime_utc_ms) {
-		return -1;
-	    }
-	    return 0; // both entries are equal
-	});
+        this.datastore.sort(function(a,b){
+            if(a.starttime_utc_ms<b.starttime_utc_ms) {
+            return 1;
+            } else if(a.starttime_utc_ms>b.starttime_utc_ms) {
+            return -1;
+            }
+            return 0; // both entries are equal
+        });
     },
 
     saveToLocalStorage:function() {
-	localStorage.tut_grill_entries=JSON.stringify(this.datastore);
+        localStorage.tut_grill_entries=JSON.stringify(this.datastore);
     },
 
     readFromLocalStorage:function() {
-	this.datastore=JSON.parse(localStorage.tut_grill_entries);
+        this.datastore=JSON.parse(localStorage.tut_grill_entries);
     }
 };
