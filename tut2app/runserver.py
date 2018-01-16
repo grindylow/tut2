18338062ad416c7a7128eb5bd7909197eb882868
Martin <martin.grill@gmx.de>
2018-01-16 20:22:47 +0100

    flask secret key now comes from the same secrets file as all the other secrets

--- tut2app/runserver.py
+++ tut2app/runserver.py
@@ -1,8 +1,9 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-logging.info("Starting...")
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("Starting...")
 
-from tut2 import app
+    from tut2 import app
+    app.run(debug=True,host='0.0.0.0')
 
-app.run(debug=True,host='0.0.0.0')
--- tut2app/tut2/__init__.py
+++ tut2app/tut2/__init__.py
@@ -1,7 +1,37 @@
 import os
+import configparser
+import string
+import random
+import logging
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
+
+logger = logging.getLogger()
+
+def get_flask_key():
+    # set a 'SECRET_KEY' to enable the Flask session cookies
+    # we read the key from a file, which we exclude from
+    # the source repository
+    key = None
+    cfg = configparser.ConfigParser()
+    cfg.read("secrets.conf")
+    if cfg.has_section('keys'):
+        s = cfg['keys']
+        if 'flask_secret_key' in s:
+            logger.info('Read existing flask secret key.')
+            key = s['flask_secret_key']
+    else:
+        cfg.add_section('keys')
+    if key is None:
+        # generate a new key and store it for future use
+        logger.info('No flask key found - generating one.')
+        cs = string.digits + string.ascii_letters + '!#$&()*+,-./:;<=>?@[]^_{|}~'
+        key = ''.join(random.choice(cs) for _ in range(20))
+        cfg['keys']['flask_secret_key'] = key
+        with open('secrets.conf','w') as f:
+            cfg.write(f)
+    return key
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -10,12 +40,7 @@ login_manager.init_app(app)
 # the toolbar is only enabled in debug mode:
 #app.debug = True
 
-# set a 'SECRET_KEY' to enable the Flask session cookies
-# we read the key from a file, which we exclude from
-# the source repository
-f=open(os.path.dirname(__file__)+"/flask_secret_key","r")
-app.config['SECRET_KEY']=f.read()
-f.close() 
+app.config['SECRET_KEY'] = get_flask_key()
 
 toolbar = DebugToolbarExtension(app)
 
