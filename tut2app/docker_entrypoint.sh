b7d143b83903a642d3d7fb9bde9a0cce01675a3d
Martin Grill <3176021+grindylow@users.noreply.github.com>
2024-11-30 14:29:57 +0100

    Working towards docker automation

--- tut2app/docker_entrypoint.sh
+++ tut2app/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e    # exit on first non-zero return code
-./setup_wizard.py -C /etc/tut2.conf.d
+poetry run ./setup_wizard.py -C /etc/tut2.conf.d
 TUT2_CONF_FILE=/etc/tut2.conf.d/tut2.conf exec gunicorn tut2app:app -b 0.0.0.0:5000 --access-logfile -
